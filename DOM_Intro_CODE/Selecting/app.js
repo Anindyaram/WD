fd12400a73d8e469093be43e2ea9c05be0a59c56
Anindya <anindyaramtripathi@gmail.com>
2022-11-26 14:58:24 +0530

    selector

--- DOM_Intro_CODE/Selecting/app.js
+++ DOM_Intro_CODE/Selecting/app.js
@@ -2,7 +2,7 @@
 // for(let i of image){
 //     img.src = 'https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80'
 // }
-
+/*
 //selects all the tag with img 
 const allImages = document.getElementsByTagName('img');
 
@@ -15,7 +15,7 @@ const squareImages = document.getElementsByClassName('square');
 for (let img of squareImages) {
     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
 }
-
+*/
 // const links = document.querySelector('p')
 const links = document.querySelectorAll('p a');
 
