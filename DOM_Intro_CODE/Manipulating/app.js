7148a86e85965e214b66d2e171b129262cbab2a2
Anindya <anindyaramtripathi@gmail.com>
2022-11-26 16:01:47 +0530

    Attribute selector in javascript

--- DOM_Intro_CODE/Manipulating/app.js
+++ DOM_Intro_CODE/Manipulating/app.js
@@ -4,9 +4,35 @@ const allLinks = document.querySelectorAll('a');
 //     link.innerText = 'I AM A LINK!!!!'
 // }
 
+// document.querySelector('p').innerHTML = '<i>lklmklm</i>'
+// document.querySelector('p').innerHTML += '<sup>lklmklm</sup>'
+
+//id
+
+// document.querySelector('#banner').id = 'try'
 
 for (let link of allLinks) {
     link.style.color = 'rgb(0, 108, 134)';
     link.style.textDecorationColor = 'magenta';
     link.style.textDecorationStyle = 'wavy'
 }
+/*
+-------------------HTML--------------------
+Egg and Chicken
+
+    
+<h4>Which Came First?</h4>
+<img src="https://devsprouthosting.com/images/egg.jpg" width="200px"></img>
+//-----------------JS----------------------
+
+const image = document.querySelector('img')
+
+image.setAttribute('src','https://devsprouthosting.com/images/chicken.jpg')
+image.setAttribute('alt','chicken')
+*/
+
+
+
+
+
+
