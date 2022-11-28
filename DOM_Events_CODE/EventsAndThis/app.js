c360091e8621bedb3edd6366615df3d46989d711
Anindya <anindyaramtripathi@gmail.com>
2022-11-28 17:38:28 +0530

    DOM Events

--- DOM_Events_CODE/EventsAndThis/app.js
+++ DOM_Events_CODE/EventsAndThis/app.js
@@ -8,15 +8,34 @@ const makeRandColor = () => {
 const buttons = document.querySelectorAll('button');
 
 for (let button of buttons) {
-    button.addEventListener('click', colorize)
+    button.addEventListener('mouseover', colorize)
 }
 
 const h1s = document.querySelectorAll('h1');
 for (let h1 of h1s) {
-    h1.addEventListener('click', colorize)
+    h1.style.marginTop = '0';
+    h1.style.marginBottom = '0';
+    h1.style.textAlign = 'center';
+    h1.addEventListener('mouseover', colorize)
 }
 
 function colorize() {
     this.style.backgroundColor = makeRandColor();
     this.style.color = makeRandColor();
 }
+
+const h2s = document.querySelector('h2');
+h2s.style.textAlign = 'center';
+h2s.style.margin = 0;
+h2s.style.padding = '20px';
+h2s.style.fontSize = '50px'
+h2s.addEventListener('mouseover',()=>{
+    h2s.style.backgroundColor=makeRandColor();
+    h2s.style.color = makeRandColor();
+})
+
+const html = querySelector('body')
+html.addEventListener('click',()=>{
+    html.style.backgroundColor = makeRandColor();
+    console.log('hello')
+})
--- DOM_Events_CODE/RandomColors/app.js
+++ DOM_Events_CODE/RandomColors/app.js
@@ -2,10 +2,16 @@ const button = document.querySelector('button');
 const h1 = document.querySelector('h1');
 
 button.addEventListener('click', function () {
+    setInterval(names,400)
+},{once:true})
+
+// button.onclick=setInterval(names,500);
+
+function names(){
     const newColor = makeRandColor();
     document.body.style.backgroundColor = newColor;
     h1.innerText = newColor;
-})
+}
 
 const makeRandColor = () => {
     const r = Math.floor(Math.random() * 255);
