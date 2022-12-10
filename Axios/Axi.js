136afe7363ac2614f0067087eabd725c241077c0
Anindya <anindyaramtripathi@gmail.com>
2022-12-10 23:54:00 +0530

    getdadjoke api fetched by axios

--- Axios/Axi.js
+++ Axios/Axi.js
@@ -1,6 +1,35 @@
-axios.get("https://swapi.dev/api/people/1/")
-.then((res)=>{
-    console.log("Response: ",res);
-});
+// axios.get("https://swapi.dev/api/people/1/")
+// .then((res)=>{
+//     console.log("Response: ",res);
+// })
+// .catch((e)=>{
+//     console.log("Error!! Yohohohohoho!! ",e)
+// })
+
+const getStarWarsPerson = async (id)=>{
+    try{
+        const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+        console.log(res.data);
+    }catch(e){
+        console.log('ERROR!!' , e);
+    }
+};
+
+getStarWarsPerson(3);
+
+
+const getDadJoke = async ()=>{
+    try{
+        const res = await axios.get("https://icanhazdadjoke.com/")
+        console.log(res)
+    }catch(e){
+        console.log("ERROR!!",e)
+    }
+}
+getDadJoke();
+
+
+
+
 
 
