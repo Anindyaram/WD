d760a882418109e79692941d99116b39db7a6b6d
Anindya <anindyaramtripathi@gmail.com>
2022-12-11 00:20:10 +0530

    fetching jokes from api

--- Axios/Axi.js
+++ Axios/Axi.js
@@ -6,30 +6,37 @@
 //     console.log("Error!! Yohohohohoho!! ",e)
 // })
 
-const getStarWarsPerson = async (id)=>{
-    try{
-        const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
-        console.log(res.data);
-    }catch(e){
-        console.log('ERROR!!' , e);
+// const getStarWarsPerson = async (id)=>{
+//     try{
+//         const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+//         console.log(res.data);
+//         console.log(res.data.name);
+//     }catch(e){
+//         console.log('ERROR!!' , e);
+//     }
+// };
+
+
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector('button');
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    console.log(jokeText)
+    //     const newLi = document.createElement('li');
+    //     newLi.append(res.data.joke);
+    //     jokes.append(newLi);
     }
-};
 
-getStarWarsPerson(3);
-
-
-const getDadJoke = async ()=>{
-    try{
-        const res = await axios.get("https://icanhazdadjoke.com/")
-        console.log(res)
-    }catch(e){
-        console.log("ERROR!!",e)
+    const getDadJoke = async () => {
+        try {
+            const config = { headers: { Accept: 'application/json' } }
+            const res = await axios.get("https://icanhazdadjoke.com/", config)
+            return res.data.joke;
+        } catch (e) {
+            console.log("ERROR!!", e)
+        }
     }
-}
-getDadJoke();
 
-
-
-
-
+    button.addEventListener('click', addNewJoke);
 
