0f5fd4c1d87c29927beda77957bd47b5d6954e8d
Anindya <anindyaramtripathi@gmail.com>
2023-01-01 15:22:35 +0530

    Installing pakages from npm and using them

--- NODE.JS/Jokester/index.js
+++ NODE.JS/Jokester/index.js
@@ -1,5 +1,16 @@
 const joke =require('give-me-a-joke')
+const oneLinerJoke = require('one-liner-joke')
+const color = require('colors')
 // console.dir(joke);
-joke.getRandomDadJoke (function(joke) {
-     console.log(joke);
-});
+// joke.getRandomDadJoke (function(joke) {
+//      console.log(joke);
+// });
+
+// var category = "knock knock";
+// joke.getRandomJokeOfTheDay (category, function(joke) {
+//      // NOTE: The service provider has made the restriction of 5 calls an hour for RandomJokeOfTheDay
+//     console.log(joke.rainbow);
+// });
+
+var getRandomJoke = oneLinerJoke.getRandomJoke();
+console.log(getRandomJoke.body.rainbow)
