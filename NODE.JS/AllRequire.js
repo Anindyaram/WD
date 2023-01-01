5f096870ffbed66f68b041cab3d42b5dd826a939
Anindya <anindyaramtripathi@gmail.com>
2023-01-01 14:20:14 +0530

    Require in node

---
+++ NODE.JS/AllRequire.js
@@ -0,0 +1,4 @@
+const cats = require('./RequireInJS');
+//Thier is a index file and whatever it exports it's ultimately going to show here or can be accessed
+
+console.log(cats);
--- NODE.JS/RequireInJS/index.js
+++ NODE.JS/RequireInJS/index.js
@@ -1,8 +1,6 @@
-const { color } = require('./blue');
 const blue = require('./blue');
 const green = require('./green');
 const yellow = require('./yellow');
 
 const colors = [blue,green,yellow];
-
-console.log(colors);
+module.exports = colors; 
