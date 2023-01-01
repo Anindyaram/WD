28d7bedc621f86cd107a6b356532e27d46afbfe0
Anindya <anindyaramtripathi@gmail.com>
2023-01-01 12:54:50 +0530

    BoilerPlate for creating a folder with HTML CSS and JavaScript file

--- NODE.JS/boilerplate.js
+++ NODE.JS/boilerplate.js
@@ -1,5 +1,8 @@
 const fs = require('fs');   //import a built-in module with the help of require in the code
-const folderName = process.argv[2] || 'Project'
+const folderName = process.argv[2] || 'Project';
+const htmlFile = process.argv[3] || 'Index.html';
+const cssFile = process.argv[4] || 'style.css';
+const JsFile = process.argv[5] || 'app.js';
 
 // fs.mkdir('Dogs', { recursive: true }, (err) => {
 //     console.log("IN THE CALLBACK!!")
@@ -8,9 +11,12 @@ const folderName = process.argv[2] || 'Project'
 
 try {
     fs.mkdirSync(folderName);
-    fs.writeFileSync(`${folderName}/index.html`, '')
-    fs.writeFileSync(`${folderName}/app.js`, '')
-    fs.writeFileSync(`${folderName}/styles.css`, '')
+    // fs.writeFileSync(`${folderName}/index.html`, '')
+    // fs.writeFileSync(`${folderName}/app.js`, '')
+    // fs.writeFileSync(`${folderName}/styles.css`, '')
+    fs.writeFileSync(`${folderName}/${htmlFile}`, '');
+    fs.writeFileSync(`${folderName}/${cssFile}`, '');
+    fs.writeFileSync(`${folderName}/${JsFile}`, '');
 } catch (e) {
     console.log("SOMETHING WENT WRONG!!!");
     console.log(e);
