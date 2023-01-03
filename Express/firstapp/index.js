44b640ee0a73f779c3b5e63b8e31c593b0a8fc1b
Anindya <anindyaramtripathi@gmail.com>
2023-01-03 13:04:31 +0530

    first server created in expressJS

--- Express/firstapp/index.js
+++ Express/firstapp/index.js
@@ -2,7 +2,13 @@ const express = require("express")
 const app = express();
 // console.dir(app)
 
+//whenever 
+app.use(()=>{
+    console.log("I got a request 😊")
+})
+
+//connects to the localhost:3000
 app.listen(3000,()=>{
     console.log("Listening at port 3000");
-})
+}) 
 
