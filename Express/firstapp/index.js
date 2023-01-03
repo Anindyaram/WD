dabcd978cd1ed6c318e856cbfa000df559310c6d
Anindya <anindyaramtripathi@gmail.com>
2023-01-03 13:34:06 +0530

    Express app

--- Express/firstapp/index.js
+++ Express/firstapp/index.js
@@ -2,10 +2,12 @@ const express = require("express")
 const app = express();
 // console.dir(app)
 
-//whenever 
-app.use(()=>{
-    console.log("I got a request 😊")
-})
+//whenever thier is a request then use runs automatically
+app.use((req,res)=>{
+    console.log("I got a request 😊");
+    // res.status(404).send("I have a bad news for you 43:32:65:23")
+    res.send("<h1>Their's a bad news for you guys!!</h1>")
+})  
 
 //connects to the localhost:3000
 app.listen(3000,()=>{
