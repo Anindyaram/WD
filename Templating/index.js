eb188e3dcbffbbfa6d945dd1859fc3a8acb9bad9
Anindya <anindyaramtripathi@gmail.com>
2023-01-26 14:41:38 +0530

    EJS condition statements

--- Templating/index.js
+++ Templating/index.js
@@ -1,27 +1,30 @@
 const express = require('express')
-const path = require('path');
+const path = require('path'); 
 const app = express();
 
 app.set('view engine' , 'ejs'); //setting ejs 
 
+//Use to join the paths. So that index.js can access views file even when it is called from outside the current directory 
 app.set('views' , path.join(__dirname , '/views'))
 
 app.get('/',(req ,res)=>{
     console.log("Sends the webpage to localhost:3000")
     res.render('home.ejs');
 })
-//Sending data by templating
+//Sending data by templating https://localhost:3000/rand
 app.get('/rand' , (req , res)=>{
     console.log('Random page accessed!');
-    const num = Math.floor(Math.random()*10)+1;
-    res.render('subpage.ejs' , {rand : num});
+    const num = Math.floor(Math.random()*10)+1; // Data that we are going to send from route to Embedded JavaScript
+    res.render('rand.ejs' /*File that we have to access*/ , {num : num}/*Object:key value pair as 2nd argument*/);
 })
 //created a sub page by using a pattern for pages like https://localhost:3000/subpage
-app.get('/:pages' , (req ,res)=>{
+app.get('/r/:subpage' , (req ,res)=>{
     console.log('Entered sub page!');
-    res.send('Sorry This Sub Page is not available');
+    const {subpage} = req.params;
+    res.render('subpage.ejs' , {subpage});
 })
 
+//Connecting to https://localhost:3000/
 app.listen(3000 , ()=>{
     console.log('Listening on port 3000');
 })
