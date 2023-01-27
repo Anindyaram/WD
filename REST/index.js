0f8a360403a6168a71911e8dbb880d08841d518f
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-01-27 19:57:19 +0530

    Parsing the data from request

--- REST/index.js
+++ REST/index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
 
+//runs for every request
+app.use(express.urlencoded({extended:true}));
+
 app.get('/tacos' , (req,res)=>{
-    console.log('tacos accessed')
+    console.log("get request from /tacos")
     res.send('/tacos started')
 })
 
 app.post('/tacos' , (req,res)=>{
     console.log('this is a post request!')
-    const a = {
-        reqType: 'POST',
-        paths: '/tacos'
-    }
+    const a = {reqType: 'POST',paths: '/tacos'}
     res.send(a.reqType );
+    console.log(req.body);
 
 })
 
