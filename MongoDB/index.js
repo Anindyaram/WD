dbf5aee341d01c3cff17507fa471d286ad08672d
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-02-03 23:49:01 +0530

    Added data from js file to mongo

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/test',)
+mongoose.connect('mongodb://127.0.0.1:27017/movieApp',)
     .then(()=>{
         console.log("Connection!")
     })
@@ -8,3 +8,22 @@ mongoose.connect('mongodb://127.0.0.1:27017/test',)
         console.log("Error ")
         console.log(err)
     })
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating:String
+});
+
+const Movie = mongoose.model('Movie' ,movieSchema);
+const amadeus = new Movie({title:'Amadeus',year:1988 ,score:9.2 ,rating:'R'});
+
+
+//run mongod in background 
+// run node 
+// inside node -> .load index.js(this will run index.js)
+// we can access instances like amadeus
+//type amadeus.save() ->to save the amadeus instance to mongo
+//now go to powershell and run mongosh (which is mongo shell)
+//inside mongosh now we see amadeus data
