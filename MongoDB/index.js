ccaa0f4636cb88173cc73d3a1dd678bd54bf3e8b
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-02-06 22:55:16 +0530

    Added my error in product.js

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-mongoose.set('useFindAndModify',false);
+// mongoose.set('useFindAndModify',false);
 //connecting to the database running in the background
 mongoose.connect('mongodb://127.0.0.1:27017/movieApp',)
     .then(()=>{
--- MongoDB/product.js
+++ MongoDB/product.js
@@ -17,21 +17,55 @@ const shopSchema = new mongoose.Schema({
     },
     price:{
         type:Number, //validation that can be convert into a number
+        required:true,
+        min:[0,'Hey dude! Price must be positive'] //Added my error message
+    },
+    onSale:{
+        type:Boolean,
+        default:false
+    },
+    categories:[String],    //Array storing
+    qty:{                   //Nested query 
+        online:{
+            type:Number,
+            default:0
+        },
+        inStore:{
+            type:Number,
+            default:0
+        }
+    },
+    size:{
+        type:String,
+        enum:['S','M','L']
     }
 })
 
 const Shop = mongoose.model('Shop', shopSchema);
 
-const vegies = new Shop({price:20});
-vegies.save()
-    .then(n =>{
-        console.log('Worked just fine!');
-        console.log(n);
-    })
-    .catch(err=>{
-        console.log('ERROR oh boi!!');
-        console.log(err.errors.name.properties.message);
-    })
+//Creating new data with validation
+// const vegies = new Shop({name:'Tire pump' ,price:40.5 ,categories:['Cyclic']})
+// vegies.save()
+//     .then(n =>{
+//         console.log('Worked just fine!');
+//         console.log(n);
+//     })
+//     .catch(err=>{
+//         console.log('ERROR oh boi!!');
+//         console.log(err.errors.name.properties.message);
+//     })
+
+// Shop.findOneAndUpdate({name:'Tire pump'} , {price:-9},{new:true}) //Updated to -9 even after validation
+Shop.findOneAndUpdate({name:'Tire pump'} , {price:-9},{new:true , runValidators:true}) //Now validation will be checked and -9 will not appended to data
+        .then(n =>{
+            console.log('Worked just fine!');
+            console.log(n);
+        })
+        .catch(err=>{
+            console.log('ERROR oh boi!!');
+            console.log(err.errors);
+            // console.log(err.errors.name.properties.message);
+        })
 
 // Shop.insertMany([
 //     {name:'rice' ,price:40},
