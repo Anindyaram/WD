6bc17639ef7c5d0c98230ed559c556121d10cb19
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-02-07 00:31:42 +0530

    Middleware concept used

--- MongoDB/product.js
+++ MongoDB/product.js
@@ -41,7 +41,40 @@ const shopSchema = new mongoose.Schema({
     }
 })
 
+//I can access this on every instance of product
+shopSchema.methods.greet = function(){
+    console.log("Namaskar!");
+    console.log(`-from ${this.name}`)
+}
+//for changing the sale we can make a method like
+shopSchema.methods.toggleSale=function(){
+    this.onSale = !this.onSale;
+    return this.save(); //takes time
+}
+//Adding categories 
+shopSchema.methods.addCategory = function(newCat){
+    this.categories.push(newCat);
+    return this.save();
+}
+
+productSchema.statics.fireSale = function () {
+    return this.updateMany({}, { onSale: true, price: 0 })
+}
+
+
 const Shop = mongoose.model('Shop', shopSchema);
+
+// Finding a product and i have greet method accessible on it
+const findProduct = async ()=>{
+    const foundProduct = await Shop.findOne({name:'Tire pump'});
+    console.log(foundProduct) ;
+    await foundProduct.toggleSale();
+    console.log(foundProduct);
+    await foundProduct.addCategory('Cringe');
+    console.log(foundProduct);
+}
+findProduct();
+
 
 //Creating new data with validation
 // const vegies = new Shop({name:'Tire pump' ,price:40.5 ,categories:['Cyclic']})
@@ -55,17 +88,18 @@ const Shop = mongoose.model('Shop', shopSchema);
 //         console.log(err.errors.name.properties.message);
 //     })
 
+//updating with validation
 // Shop.findOneAndUpdate({name:'Tire pump'} , {price:-9},{new:true}) //Updated to -9 even after validation
-Shop.findOneAndUpdate({name:'Tire pump'} , {price:-9},{new:true , runValidators:true}) //Now validation will be checked and -9 will not appended to data
-        .then(n =>{
-            console.log('Worked just fine!');
-            console.log(n);
-        })
-        .catch(err=>{
-            console.log('ERROR oh boi!!');
-            console.log(err.errors);
-            // console.log(err.errors.name.properties.message);
-        })
+// Shop.findOneAndUpdate({name:'Tire pump'} , {price:-9},{new:true , runValidators:true}) //Now validation will be checked and -9 will not appended to data
+        // .then(n =>{
+        //     console.log('Worked just fine!');
+        //     console.log(n);
+        // })
+        // .catch(err=>{
+        //     console.log('ERROR oh boi!!');
+        //     console.log(err.errors);
+        //     // console.log(err.errors.name.properties.message);
+        // })
 
 // Shop.insertMany([
 //     {name:'rice' ,price:40},
