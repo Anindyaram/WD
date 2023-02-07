0305d9af9b78ce9a76a5cfcfc7c0c0ceb94e2d9e
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-02-07 19:49:55 +0530

    Middleware

--- MongoDB/person.js
+++ MongoDB/person.js
@@ -1,3 +1,4 @@
+//Middleware runs automatically it depends when and where we want to use them. We can run it before or after any function or when something is done on user side ,etc
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -16,16 +17,17 @@ const personSchema = new mongoose.Schema({
 personSchema.virtual('fullName').get(function () {
     return `${this.first} ${this.last}`
 })
-
+//This is called a middleware which runs before the save function automatically
 personSchema.pre('save', async function () {
     this.first = 'YO';
     this.last = 'MAMA';
     console.log("ABOUT TO SAVE!!!!")
 })
+
+//This is called a middleware which runs after the save function automatically
 personSchema.post('save', async function () {
     console.log("JUST SAVED!!!!")
 })
-
 
 const Person = mongoose.model('Person', personSchema);
 
